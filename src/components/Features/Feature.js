7b9b9c637ea7075a1bfb0e7b522d7797cd41e885
Al-Xafis <alhafiz@mail.ru>
2020-11-12 02:21:40 +0500

    finished features

--- src/components/Features/Feature.js
+++ src/components/Features/Feature.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import Button from '../Button/Button';
 
-const Feature = () => {
-  return (
-  <div className="feature">
-    <div className="feature__description">
-      <h3>Add a cart platform to your favorite website stack</h3>
-      <p>
-        Enable e-commerce on a WordPress blog, an existing site, a SPA, a JAMstack project... anything! 
-        Use CMS & frameworks you love, and let our third party shopping cart do the heavy lifting.
-      </p>
-      <Button text="Learn More" classh="feature__button"/>
-    </div>
-    <div className="feature__pic">
-      <img scr="" alt="feature_pic"/>
-    </div>
-  </div>
-  );
+const Feature = ({ left }) => {
+  if (left) {
+    return (
+      <div className="feature">
+        <div className="feature__description">
+          <h3>Add a cart platform to your favorite website stack</h3>
+          <p>
+            Enable e-commerce on a WordPress blog, an existing site, a SPA, a JAMstack project... anything! 
+            Use CMS & frameworks you love, and let our third party shopping cart do the heavy lifting.
+          </p>
+          <Button text="Learn More" classh="feature__button"/>
+        </div>
+        <div className="feature__pic">
+          <img src="/pics/dollar-coin.svg" alt="feature_pic"/>
+        </div>
+        
+      </div>
+      );
+  } else {
+    return (
+      <div className="feature">
+        <div className="feature__pic">
+          <img src="/pics/dollar-coin.svg" alt="feature_pic"/>
+        </div>
+        <div className="feature__description">
+          <h3>Add a cart platform to your favorite website stack</h3>
+          <p>
+            Enable e-commerce on a WordPress blog, an existing site, a SPA, a JAMstack project... anything! 
+            Use CMS & frameworks you love, and let our third party shopping cart do the heavy lifting.
+          </p>
+          <Button text="Learn More" classh="feature__button"/>
+        </div>
+      </div>
+      );
+  }
+  
 };
 
 export default Feature;
